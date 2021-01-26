8979ff4c5cae9ea444aefef5bff5712b29a0367b
ErinnGentry <gentryerinn@gmail.com>
2021-01-26 13:52:45 -0800

    Updated Prework

---
+++ Prework/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by ErinnG on 1/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UITextField!
+    
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.title = "Tip Calculator"
+        
+    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        if segue.destination is SettingsViewController {
+//            let customTips = segue.destination as? SettingsViewController
+//
+//        }
+//    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+    // Get bill amount from text field
+        let bill = Double(billAmountTextField.text!) ?? 0
+        // Get total tip
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        // Update Tip Amount Label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+        
+
+}
+
